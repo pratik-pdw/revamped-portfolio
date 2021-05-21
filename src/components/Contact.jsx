1769f66c430f0067974cf2dc6166689286a9073c
Wadekar <503145942@ge.com>
2021-05-21 22:26:48 +0530

    contact me section updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 import { IoMail } from 'react-icons/io5'
 import { FaLinkedin, FaFacebookSquare, FaInstagram, FaGithub } from 'react-icons/fa'
 function Contact() {
+
+    const socialLinks = [
+        {
+            icon: <IoMail />,
+            url: 'pratik.wadekar2810@gmail.com',
+            isMail: true,
+            username: 'pratik.wadekar2810@gmail.com'
+        },
+        {
+            icon: <FaLinkedin />,
+            url: 'https://linkedin.com/in/mr-pdw',
+            username: 'mr-pdw'
+        },
+        {
+            icon: <FaGithub />,
+            url: 'https://github.com/pratik-pdw',
+            username: 'pratik-pdw'
+        },
+        {
+            icon: <FaFacebookSquare />,
+            url: 'https://www.facebook.com/pratikw.2810',
+            username: 'pratikw.2810'
+        },
+        {
+            icon: <FaInstagram />,
+            url: 'https://instagram.com/pratikwdkr',
+            username: 'pratikwdkr'
+        }
+    ]
+
+
+
     return (
         <section className="section-contact">
             <svg className="curve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -15,33 +47,16 @@ function Contact() {
             </p>
 
             <ul className="links">
-                <li className="links_item">
-                    <a className="links_url" href="#!">  <span className="links__icon"><IoMail /></span>
-                        <p>pratik.wadekar2810@gmail.com</p></a>
+                {socialLinks.map(link => {
+                    return <li key={link.url} className="links_item">
+                        <a rel="noreferrer" target="_blank" className="links_url" href={
+                            link.isMail ? 'mailto:' + link.url : link.url
+                        }>  <span className="links__icon">{link.icon}</span>
+                            <p>{link.username}</p></a>
 
-                </li>
-                <li className="links_item">
-                    <a className="links_url" href="#!"> <span className="links__icon"><FaLinkedin /></span>
-                        <p>mr-pdw</p></a>
+                    </li>
+                })}
 
-                </li>
-                <li className="links_item">
-                    <a className="links_url" href="#!">  <span className="links__icon"><FaGithub /></span>
-                        <p>pratik-pdw</p></a>
-
-                </li>
-
-
-                <li className="links_item">
-                    <a className="links_url" href="#!"> <span className="links__icon"><FaFacebookSquare /></span>
-                        <p>pratikw.2810</p></a>
-
-                </li>
-                <li className="links_item">
-                    <a className="links_url" href="#!"> <span className="links__icon"><FaInstagram /></span>
-                        <p>pratikwdkr</p></a>
-
-                </li>
 
             </ul>
 
