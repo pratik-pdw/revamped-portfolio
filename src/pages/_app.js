23e058069ad4ffb5574b76299a8218559e9c2ad9
Pratik Wadekar <pratik.wadekar@globant.com>
2022-04-14 23:04:05 +0530

    nextjs migration

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+import "../sass/main.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
